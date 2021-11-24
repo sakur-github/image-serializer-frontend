06e891497ca4d53a95a5dee76b733f28fb960ee5
oliverlevay <oliver.levay@hotmail.com>
2021-11-24 11:26:03 +0100

    cleaner syntax

--- src/closestMultipleOf.ts
+++ src/closestMultipleOf.ts
@@ -1,17 +1,11 @@
 export const closestMultipleOf = (multiple: number, comparison: number) => {
   let closestDown = Math.round(comparison);
-  while (closestDown % multiple !== 0) {
-    closestDown -= 1;
-  }
+  while (closestDown % multiple !== 0) closestDown -= 1;
 
   let closestUp = Math.round(comparison);
-  while (closestUp % multiple !== 0) {
-    closestUp += 1;
-  }
+  while (closestUp % multiple !== 0) closestUp += 1;
 
-  if (Math.abs(closestDown - comparison) < Math.abs(closestUp - comparison)) {
+  if (Math.abs(closestDown - comparison) < Math.abs(closestUp - comparison))
     return closestDown;
-  } else {
-    return closestUp;
-  }
+  else return closestUp;
 };
