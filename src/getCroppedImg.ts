ca96f56bb92b97209947e971dc8633b5daabcc1f
oliverlevay <oliver.levay@hotmail.com>
2021-11-24 03:52:38 +0100

    more attempts to fix mobile issues

--- src/getCroppedImg.ts
+++ src/getCroppedImg.ts
@@ -8,11 +8,11 @@ export function getCroppedImg(image: HTMLImageElement, crop: Crop) {
   canvas.height = crop.height;
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-  // New lines to be added
+  /*   // New lines to be added
   const pixelRatio = window.devicePixelRatio;
   canvas.width = crop.width * pixelRatio;
   canvas.height = crop.height * pixelRatio;
-  ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+  ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0); */
   ctx.imageSmoothingQuality = "high";
 
   ctx.drawImage(
